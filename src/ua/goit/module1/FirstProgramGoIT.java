370aa4dff1fec206a3866a11c671207897a28891
githubua <interni7@gmail.com>
2016-03-18 15:26:37 +0200

    Initial commit

---
+++ src/ua/goit/module1/FirstProgramGoIT.java
@@ -0,0 +1,7 @@
+package ua.goit.module1;
+
+public class FirstProgramGoIT {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java Program! Stay tuned!");
+    }
+}
